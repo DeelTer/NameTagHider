c564f44e8ee5dcfb6f01d67b7489f8a587101af2
Teleport <77507478+teleport2@users.noreply.github.com>
2023-03-31 19:33:09 +0300

    currently show name onclick player

--- src/main/java/ru/deelter/namehider/NameHideListener.java
+++ src/main/java/ru/deelter/namehider/NameHideListener.java
@@ -1,7 +1,11 @@
 package ru.deelter.namehider;
 
+import net.kyori.adventure.text.Component;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.jetbrains.annotations.NotNull;
 
@@ -10,5 +14,14 @@ public class NameHideListener implements Listener {
     @EventHandler
     public void onJoin(@NotNull PlayerJoinEvent event) {
         NameHider.getTeam().addEntry(event.getPlayer().getName());
+    }
+
+    @EventHandler
+    public void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
+        Entity interactedPlayer = event.getRightClicked();
+
+        if (interactedPlayer instanceof Player) {
+            event.getPlayer().sendActionBar(Component.text("§6-=[§f" + interactedPlayer.getName() + "§6]=-"));
+        }
     }
 }
